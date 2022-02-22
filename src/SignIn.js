f1c99f2ab467626f2710277a5915ac37ba75e244
Matt Beckwith <mbeckwithfl@yahoo.com>
2022-02-21 20:44:59 -0700

    welcome page

---
+++ src/SignIn.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function SignIn() {
+  return (
+    <div className='SignIn'>
+       Sign In
+    </div>
+  );
+}
+
+export default SignIn;
---
+++ src/Welcome.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function Welcome() {
+  return (
+    <div className='center'>
+    <div className="Welcome">
+      <p> Welcome! </p> 
+      <p><a href='https://www.google.com' target="_blank">Sign In</a></p>
+      <p1>No Account?</p1>
+      <p><a href='https://www.google.com' target="_blank">Sign Up</a></p>
+    </div>
+    
+    
+    </div>
+  );
+}
+
+export default Welcome;
